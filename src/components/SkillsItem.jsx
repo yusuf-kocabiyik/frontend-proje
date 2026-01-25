5a300b29255f0d2ea4703827abc3818c6ca63f7f
yusuf-kocabiyik <kocabiyikyusuf3@gmail.com>
2026-01-25 05:06:19 +0300

    test
    test

--- src/components/SkillsItem.jsx
+++ src/components/SkillsItem.jsx
@@ -4,7 +4,7 @@ export const SkillsItem =({skill})=>{
 
     return(
         <>
-            <div className="flex flex-col  gap-4 w-95">
+            <div className="flex flex-col  gap-4 w-93">
             <h2 className="text-3xl text-center lg:text-left lg:text-3xl text-indigo1 font-medium  dark:text-darkTitle2 " >{skill.skillName}</h2>
             <p  className="text-base lg:text-sm lg:text-left font-normal  text-gray1 dark:text-[#FFFFFF]">{skill.description}</p>
             </div>
